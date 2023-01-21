cb6dcf8400de5d6678fb22b88556190ce7c3b469
IvanZH <ivzh.nn@gmail.com>
2023-01-21 16:09:05 +0300

    task13 completed in a folder task13-1

---
+++ task13-1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или 
+// сообщает, что третьей цифры нет
+Console.WriteLine("Введите трехзначное число");
+int a=Convert.ToInt32(Console.ReadLine());
+if (a<0)
+{
+    a=a*(-1);
+}
+if (a<100)
+{
+    Console.WriteLine("Третья цифра отсутствует");
+}
+else if (a>99)
+{
+    string b=Convert.ToString(a);
+    Console.WriteLine(b[2]);
+}
