544ab1b14f657bb14db0a22266329533cac458a7
IvanZH <ivzh.nn@gmail.com>
2023-01-21 16:43:23 +0300

    task 15 completed

---
+++ task15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число от 1 до 7, обозначающее день недели");
+int a=Convert.ToInt32(Console.ReadLine());
+if (a<1 || a>7)
+{
+    Console.WriteLine("Ошибка ввода");
+}
+else if (a<5)
+{
+    Console.WriteLine("Данный день недели не является выходным днем");
+}
+else
+{
+    Console.WriteLine("Данный день недели является выходным днем");
+}
